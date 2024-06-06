be03aa664a92dbd7b58d7a22c6b9fbbd132ea9be
GFazorR <comandantegr1@gmail.com>
2024-06-06 10:17:05 +0200

    added comments to utils.py

--- src/backend/utils.py
+++ src/backend/utils.py
@@ -1,7 +1,15 @@
+"""
+This module provides utility functions for the application.
+"""
 from fastapi import Request
 
 
 def get_engine(request: Request):
+    """
+    Utility function that returns mongodb engine instance in Fastapi
+    :param request:
+    :return:
+    """
     engine = request.app.engine
     return engine
 
