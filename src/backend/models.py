c7e7ee6d72ff39c380667de731b8025e184c0b3d
GFazorR <comandantegr1@gmail.com>
2024-06-06 10:01:36 +0200

    added comments to models.py

--- src/backend/models.py
+++ src/backend/models.py
@@ -1,12 +1,21 @@
+"""
+This module contains the data models used in the application.
+"""
 import datetime
-from typing import List, Union, Optional
+from typing import List
 
-from odmantic import Model, EmbeddedModel, Reference
-from pydantic import BaseModel, Field, ConfigDict
 from bson import ObjectId
+from odmantic import Model, EmbeddedModel
+from pydantic import BaseModel, Field, ConfigDict, Extra
+
+from src.backend import event
 
 
 class UserModel(Model):
+    """
+    This class represents the user model.
+    :param username: str    :param role: str
+    """
     username: str
     role: str
 
@@ -16,12 +25,24 @@ class UserModel(Model):
 
 
 class DayCapacityModel(EmbeddedModel):
+    """
+    This class represents the day capacity model.
+    :param day: datetime
+    :param max_capacity: int
+    """
     day: datetime.datetime
     max_capacity: int
 
 
-# TODO make model flexible
-class EventModel(Model):
+class EventModel(Model, extra=Extra.allow):
+    """
+    This class represents the event model.
+    :param owner: List[ObjectId]
+    :param name: str
+    :param published: bool
+    :param capacity_by_day: List[DayCapacityModel]
+    :param start_datetime: datetime    :param end_datetime: datetime
+    """
     owner: List[ObjectId]
     name: str
     published: bool
@@ -34,25 +55,36 @@ class EventModel(Model):
 
 
 class Telemetry(BaseModel):
+    """
+    This class represents the telemetry model.
+    :param participants: int
+    """
     model_config = ConfigDict(arbitrary_types_allowed=True, populate_by_name=True)
     participants: int = Field(default_factory=int)
 
 
-# # Data model for user login information
-# class Login(BaseModel):
-#     username: str
-#     password: str
-#
-
-
 class Ticket(BaseModel):
+    """
+    This class represents the ticket model.
+    :param user_id: str    :param event_id: str
+    :param price: float    :param discount: float
+    """
     model_config = ConfigDict(arbitrary_types_allowed=True, populate_by_name=True)
     user_id: str
+    event_id: str
     price: float
     discount: float
 
 
 class UserSession(BaseModel):
+    """
+    This class represents the user session model.
+    :param id: str    :param user_id: str
+    :param user_profile: UserModel
+    :param event: EventModel
+    :param ticket_sale: TicketModel
+    :param event_telemetry: TelemetryModel
+    """
     model_config = ConfigDict(arbitrary_types_allowed=True, populate_by_name=True)
     id: str = Field(alias="_id")
     user_id: str
