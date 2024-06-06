fbd5ce715ab86deafa8f4d1bd7fca0fcdf5fdd3b
GFazorR <comandantegr1@gmail.com>
2024-06-06 09:47:50 +0200

    added comments to user_session

--- src/backend/user_session.py
+++ src/backend/user_session.py
@@ -1,15 +1,23 @@
-import uuid
+"""
+This module implements user session crud operations endpoints
+"""
 from http.client import HTTPException
 
 from fastapi import APIRouter, status, HTTPException, Response
-from models import EventModel
+
 import redis_utils
+from models import EventModel
 
 router = APIRouter()
 
 
 @router.get("/session/events/{user_id}")
 async def get_session(user_id: str):
+    """
+    Retrieves the session associated with a given user ID.
+    :param user_id: str
+    :return: EventModel | HTTPException
+    """
     event = await redis_utils.get_event_session(user_id)
     if not event:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Event not found")
@@ -19,6 +27,11 @@ async def get_session(user_id: str):
 @router.put('/session/events/{user_id}')
 @router.post("/session/events/{user_id}")
 async def create_session(user_id: str, event: EventModel):
+    """
+    Performs an upsert for an EventModel session associated with a given user ID.
+    :param user_id: str
+    :param event: EventModel    :return:  | HTTPException
+    """
     try:
         await redis_utils.upsert_event_session(user_id=user_id, event=event)
     except Exception as e:
@@ -28,6 +41,11 @@ async def create_session(user_id: str, event: EventModel):
 
 @router.delete("/session/events/{user_id}")
 async def delete_session(user_id: str):
+    """
+    Deletes the session associated with a given user ID.
+    :param user_id: str
+    :return: Response | HTTPException
+    """
     event_id = await redis_utils.delete_event_session(user_id)
     if not event_id:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Event not found")
