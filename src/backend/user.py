c7851777f6461cfec7f49f85fff5a8ac52d5d58c
GFazorR <comandantegr1@gmail.com>
2024-06-06 10:15:47 +0200

    added comments to users.py

--- src/backend/user.py
+++ src/backend/user.py
@@ -1,24 +1,25 @@
-from urllib.request import Request
+"""
+This module implements endpoints for crud operations over users.
+"""
 
-from fastapi import FastAPI, APIRouter, Request, Depends, Response, status
-from redis_utils import create_session, get_session
-from models import UserModel, EventModel
-from odmantic import AIOEngine
-from typing import Annotated
 from bson import ObjectId
-import uuid
+from fastapi import APIRouter, Depends, Response, status
+
+from models import UserModel
+from redis_utils import create_session
 from utils import get_engine
 
 router = APIRouter()
 
 
-
-
-
-@router.post(
-    "/register/business",
-)
+@router.post("/register/business")
 async def register(username: str, engine=Depends(get_engine)):
+    """
+    Create a new business user.
+    :param username: str
+    :param engine: Depends on get_engine
+    :return: UserModel
+    """
     # TODO check user exists
     user = UserModel(username=username, role='business')
     user = await engine.save(user)
@@ -27,15 +28,18 @@ async def register(username: str, engine=Depends(get_engine)):
 
 @router.get('/user/business/{user_id}')
 async def get_user(user_id: str, engine=Depends(get_engine)):
+    """
+    Get the business user by id.
+    :param user_id: str
+    :param engine: Depends on get_engine
+    :return: Response
+    """
     # TODO generate user_session token/id
     user = await engine.find_one(UserModel, {'_id': ObjectId(user_id)})
     session_token = await create_session({'user': user.model_dump_json()})
     return Response(status_code=status.HTTP_200_OK,
                     headers={'X-Session-Id': session_token},
                     content=user.model_dump_json())
-
-
-
 
 
 if __name__ == '__main__':
