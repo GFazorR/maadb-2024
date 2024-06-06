f89ee94cad86fd5391b8611bdbf231a177c9a4f2
GFazorR <comandantegr1@gmail.com>
2024-06-06 09:52:50 +0200

    added comments to main.py

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,21 +1,22 @@
-import json
-import logging
-import uuid
-from typing import List, Annotated, Union
+"""
+This module is the main entry point for the application backend.
+"""
 
-from bson.objectid import ObjectId
-from fastapi import FastAPI, status, Response, Header, Depends, Cookie, BackgroundTasks
-from fastapi.responses import JSONResponse
+import logging
+
+from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
 from odmantic import AIOEngine
-from odmantic.exceptions import DocumentNotFoundError
 
-from models import Ticket, EventModel, UserModel
-import user
 import event
-import tickets
 import telemetry
+import tickets
+import user
 import user_session
+
+"""
+Initialize logging, mongodb and FastApi routing.
+"""
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -32,65 +33,35 @@ app.include_router(telemetry.router)
 app.engine = engine
 
 
-# templates = Jinja2Templates(directory="templates")
-# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
-#
-# # Mock Redis client for development purposes (replace with actual connection)
-# redis_client = redis.Redis(host='localhost', port=6379, db=0)
-# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+# TODO: query mongo and put cached data to redis
+
+@app.on_event('startup')
+async def startup():
+    """
+    This function is called when the app is first started.
+    Queries mongodb and caches the result in redis.
+    :return: None
+    """
+    # query monogo
+    # cache on redis
+    pass
 
 
-# def hash_password(password: str) -> str:
-#     hashed_password = pwd_context.hash(password)
-#     print(f"Hashed password: {hashed_password}")  # Log hashed password for debugging (remove in production)
-#     return hashed_password
+@app.on_event('shutdown')
+async def shutdown():
+    """
+    This function is called when the app shutdown.
+    Persists redis dataa in mongodb database.
+    :return: None
+    """
+    # persist on mongo
+    pass
 
 
-# # Endpoint for user registration
-# @app.post("/register")
-# async def register(user: User):
-#     hashed_password = hash_password(user.password)
-#     users_collection = app.maadb_database['maadb_users']  # Replace with your actual collection name
-#     result = users_collection.insert_one({"username": user.username, "hashed_password": hashed_password})
-#     if result.inserted_id:
-#         return {"message": "User created successfully!"}
-#     else:
-#         raise HTTPException(status_code=400, detail="User registration failed")
-#
-#
-# # Endpoint for user login with JWT authentication
-# @app.post("/login")
-# async def login(user: Login = Depends(oauth2_scheme)):
-#     print("serving Login page")
-#     users_collection = app.maadb_database['maadb_users']  # Replace with your actual collection name
-#     found_user = users_collection.find_one({"username": user.username})
-#
-#     if not found_user:
-#         raise HTTPException(status_code=400, detail="Invalid username or password")
-#
-#     if not pwd_context.verify(user.password, found_user["hashed_password"]):
-#         raise HTTPException(status_code=400, detail="Invalid username or password")
-#
-#     # JWT logic for access token generation and session management (omitted for brevity)
-#
-#     # Redirect to user profile on successful login (adapt based on your needs)
-#     response = Response()
-#     response.headers["Location"] = "/user_profile"
-#     response.status_code = status.HTTP_302_FOUND  # Set redirect status code
-#     return response
-
-
-# ----------------------- Event Creation --------------------------------
-
-# Persistence (MongoDB)
-
-
-# ----------------------- Ticket Sale --------------------------------
-
-# Persistence (Cassandra)
-
-
-# Endpoint to render the registration page as the default view
 @app.get("/")
 async def read_root():
+    """
+    Default endpoint.
+    :return: Response
+    """
     return {'message': 'Hello World'}
